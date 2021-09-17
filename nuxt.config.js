386eedd1191bf7f11f3e6689597b815d26949361
Hirokazu Degawa <hxdegawa@gmail.com>
2021-09-18 04:11:55 +0900

    feat: integrate lazyload

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,8 +56,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
       {
         rel: 'stylesheets',
-        href:
-          'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@200,400;500;700&family=Roboto:wght@200,400;500;700&display=swap',
+        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@200,400;500;700&family=Roboto:wght@200,400;500;700&display=swap',
       },
       {
         rel: 'stylesheet',
@@ -107,6 +106,7 @@ export default {
     // '@nuxtjs/pwa',
     '@nuxtjs/markdownit',
     '@nuxtjs/sitemap',
+    'nuxt-lazy-load',
   ],
 
   sentry: {
